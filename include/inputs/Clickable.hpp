648028cf472999ddbcd536b10f626ea0f10aa930
TataOwO <tim0401.kao@gmail.com>
2025-04-20 17:12:57 +0800

    added Clickable objects, that should completely decouples from any other objects

---
+++ include/inputs/Clickable.hpp
@@ -0,0 +1,56 @@
+#ifndef CLICKABLE_HPP
+#define CLICKABLE_HPP
+
+#include "hitboxes/I_BaseHitbox.hpp"
+
+#include <functional>
+#include <memory>
+
+namespace input {
+
+class Clickable final {
+public:
+	Clickable();
+public:
+	// returns true when the given object gives a delete signal
+	[[nodiscard]] bool shall_be_removed() const {
+		return removal_func
+			&& removal_func();
+	}
+
+	// calls on_click() if clicked
+	[[nodiscard]] bool process_click(const glm::vec2 click_pos) const {
+		return on_click
+			&& m_hitbox->contains_point(click_pos)
+			&& on_click();
+	}
+
+	// function setter
+	void set_removal(const std::function<bool()> &func) {removal_func = func;}
+	void set_on_click(const std::function<void()> &func) {on_click = func;}
+	void set_hitbox(const std::shared_ptr<hitboxes::I_BaseHitbox> &hb) {m_hitbox=hb;}
+private:
+	// takes function from object
+	std::function<void()> on_click;
+
+	// takes function from object too
+	std::function<bool()> removal_func;
+
+	std::shared_ptr<hitboxes::I_BaseHitbox> m_hitbox;
+
+// base
+public:
+	Clickable(const Clickable&) = delete;
+
+	Clickable(Clickable&&) = delete;
+
+	Clickable& operator=(const Clickable&) = delete;
+
+	Clickable& operator=(Clickable&&) = delete;
+
+	virtual ~Clickable() = default;
+};
+
+}
+
+#endif
