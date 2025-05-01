b7db44fe694c854e6946db9a754d2ca9b8f29b42
TataOwO <tim0401.kao@gmail.com>
2025-05-01 22:38:45 +0800

    new precompiled file

---
+++ include/precompiled.hpp
@@ -0,0 +1,17 @@
+#ifndef BTD3_PCH_HPP
+#define BTD3_PCH_HPP
+
+#include <algorithm>
+#include <array>
+#include <memory>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "../PTSD/lib/glm/glm/glm.hpp"
+
+#include <nlohmann/json.hpp>
+
+#endif
